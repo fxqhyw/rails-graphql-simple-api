9900ac0d3af04a648905f4bd178efb18f0f47b6d
Sergey Kozhemiako <therealloker@gmail.com>
2020-01-16 23:03:35 +0200

    Update singup

--- app/graphql/mutations/sign_up_user.rb
+++ app/graphql/mutations/sign_up_user.rb
@@ -3,18 +3,19 @@ module Mutations
     class AuthProviderSignupData < Types::BaseInputObject
       argument :auth_input, Types::AuthProviderInput, required: false
     end
-
     argument :nickname, String, required: true
     argument :auth_provider, AuthProviderSignupData, required: false
 
-    type Types::UserType
+    field :user, Types::UserType, null: true
+    field :errors, [String], null: true
 
     def resolve(nickname: nil, auth_provider: nil)
-      User.create!(
-        nickname: nickname,
-        email: auth_provider&.[](:auth_input)&.[](:email),
-        password: auth_provider&.[](:auth_input)&.[](:password)
-      )
+      user = User.new(nickname: nickname,
+                      email: auth_provider&.[](:auth_input)&.[](:email),
+                      password: auth_provider&.[](:auth_input)&.[](:password))
+      return { errors: user.errors.full_messages } unless user.save
+
+      { user: user }
     end
   end
 end
