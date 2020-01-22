6e526f54a587ab22ed72df1555240b9feca5b877
Sergey Kozhemiako <therealloker@gmail.com>
2020-01-22 22:49:19 +0200

    SignUp mutation tests refactor

--- spec/graphql/mutations/sign_up_user_spec.rb
+++ spec/graphql/mutations/sign_up_user_spec.rb
@@ -1,29 +1,31 @@
 RSpec.describe Mutations::SignUpUser, type: :request do
   describe '#resolve' do
-    let(:params) { { query: query, variables: variables } }
-
-    context 'when query is valid' do
-      let(:variables) { attributes_for(:user) }
-      let(:query) do
-        <<~GQL
-          mutation($nickname: String!, $email: String!, $password: String!) {
-            signUpUser(input: {
-              nickname: $nickname,
-              authInput: {
-                email: $email,
-                password: $password
-              }
-            }
-          ) {
-              user {
-                id
-                email
-              }
-              errors
+    let(:query) do
+      <<~GQL
+        mutation($nickname: String!, $email: String!, $password: String!) {
+          signUpUser(input: {
+            nickname: $nickname,
+            authInput: {
+              email: $email,
+              password: $password
             }
           }
-        GQL
-      end
+        ) {
+            user {
+              id
+              email
+            }
+            errors
+          }
+        }
+      GQL
+    end
+    let(:params) { { query: query, variables: variables } }
+    let(:response_user) { JSON.parse(response.body)['data']['signUpUser']['user'] }
+    let(:response_errors) { JSON.parse(response.body)['data']['signUpUser']['errors'] }
+
+    context 'when params is valid' do
+      let(:variables) { attributes_for(:user) }
 
       it 'creates a user' do
         expect do
@@ -33,13 +35,35 @@ RSpec.describe Mutations::SignUpUser, type: :request do
 
       it 'returns a user' do
         post '/graphql', params: params
-        json = JSON.parse(response.body)
-        user = json['data']['signUpUser']['user']
-
-        expect(user).to include(
+        expect(response_user).to include(
           'id'    => be_present,
           'email' => variables[:email]
         )
+      end
+
+      it 'does not returns any errors' do
+        post '/graphql', params: params
+        expect(response_errors).to be_nil
+      end
+    end
+
+    context 'when params is invalid' do
+      let(:variables) { { email: '', nickname: '', password: '123456' } }
+
+      it 'does not create a user' do
+        expect do
+          post '/graphql', params: params
+        end.not_to change(User, :count)
+      end
+
+      it 'does not return any user' do
+        post '/graphql', params: params
+        expect(response_user).to be_nil
+      end
+
+      it 'returns errors' do
+        post '/graphql', params: params
+        expect(response_errors).to match(["Nickname can't be blank", "Email can't be blank"])
       end
     end
   end
