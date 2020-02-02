f2c55f92305419124e1468bcc81e6f2ee6cfe707
Sergey Kozhemiako <therealloker@gmail.com>
2020-02-02 22:41:39 +0200

    implement search

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,2 @@
 User.create!(nickname: 'booker', email: 'admin@admin.com', password: '123456')
+1000.times { FactoryBot.create(:book) }
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,10 @@
 
 set -e
 
-bundle exec rails db:setup
+bundle exec rails db:create
+bundle exec rails db:migrate
+bundle exec rails db:seed
+
 bundle exec rake books:reindex
 
 exec "$@"
